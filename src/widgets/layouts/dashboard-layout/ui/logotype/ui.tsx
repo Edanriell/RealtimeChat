30659821604d871b12807d1fa65ec68d9146a29b
Edanriell <Edanriell@gmail.com>
2024-01-20 21:06:12 +0200

    decomposed dashboard page

---
+++ src/widgets/layouts/dashboard-layout/ui/logotype/ui.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export const Logotype = () => (
+	<Image
+		src="/images/chatx-logo-bubble.png"
+		alt="ChatX company logotype"
+		width={40}
+		height={40}
+	/>
+);

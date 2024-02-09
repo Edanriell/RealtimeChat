c2890f204e59e635bfad92b131e8fb81bf317afd
Edanriell <Edanriell@gmail.com>
2024-02-09 11:23:37 +0200

    refactoring input

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -7,10 +7,10 @@ import TextareaAutosize from "react-textarea-autosize";
 
 import Button from "./ui/Button";
 
-interface ChatInputProps {
+type ChatInputProps = {
 	chatPartner: User;
 	chatId: string;
-}
+};
 
 const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
 	const textareaRef = useRef<HTMLTextAreaElement | null>(null);
@@ -33,8 +33,14 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
 	};
 
 	return (
-		<div className="border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-			<div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+		<div className={"border-t border-light px-4 pt-4 mb-2 sm:mb-0"}>
+			<div
+				className={
+					"relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 " +
+					"ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600 " +
+					"bg-white"
+				}
+			>
 				<TextareaAutosize
 					ref={textareaRef}
 					onKeyDown={(e) => {
@@ -47,9 +53,12 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
 					value={input}
 					onChange={(e) => setInput(e.target.value)}
 					placeholder={`Message ${chatPartner.name}`}
-					className="block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm sm:leading-6"
+					className={
+						"block w-full resize-none border-0 bg-white text-black " +
+						"placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm " +
+						"sm:leading-6"
+					}
 				/>
-
 				<div
 					onClick={() => textareaRef.current?.focus()}
 					className="py-2"
@@ -59,13 +68,16 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
 						<div className="h-9" />
 					</div>
 				</div>
-
-				<div className="absolute right-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
-					<div className="flex-shrin-0">
-						<Button isLoading={isLoading} onClick={sendMessage} type="submit">
-							Post
-						</Button>
-					</div>
+			</div>
+			<div
+				className={
+					"absolute right-0 bottom-0 flex justify-between py-2 pl-3 pr-2"
+				}
+			>
+				<div className={"flex-shrink-0"}>
+					<Button isLoading={isLoading} onClick={sendMessage} type="submit">
+						Post
+					</Button>
 				</div>
 			</div>
 		</div>

91837a18576dd227f7f4254dcfec332583c27823
Edanriell <Edanriell@gmail.com>
2024-02-18 23:44:17 +0200

    found right colors

--- src/shared/ui/button/shapes.tsx
+++ src/shared/ui/button/shapes.tsx
@@ -76,22 +76,15 @@ export function Shapes({ isHover, isPress, mouseX, mouseY }) {
 }
 
 export function Lights() {
-	const refka = useRef<any>();
-	useShadowHelper(refka);
 	return (
 		<>
-			<spotLight color="#761beb" position={[-10, -10, -10]} intensity={220} />
-			<spotLight color="#5a14de" position={[-10, 0, 15]} intensity={880} />
-			<spotLight color="#5a14de" position={[-5, 20, 2]} intensity={550} />
-			<spotLight color="#5a14de" position={[15, 10, -2]} intensity={1200} />
-			<spotLight color="#5a14de" position={[15, 10, 5]} intensity={1100} />
-			<spotLight color="#5a14de" position={[5, -10, 5]} intensity={880} />
-			<spotLight
-				ref={refka}
-				color="#5a14de"
-				position={[-10, -20, 10]}
-				intensity={4000}
-			/>
+			<spotLight color="#5a14de" position={[-10, -10, -10]} intensity={220} />
+			<spotLight color="#2b1b9b" position={[-10, 0, 15]} intensity={880} />
+			<spotLight color="#d1caff" position={[-5, 20, 2]} intensity={550} />
+			<spotLight color="#a395ff" position={[15, 10, -2]} intensity={1200} />
+			<spotLight color="#2b1b9b" position={[15, 10, 5]} intensity={1100} />
+			<spotLight color="#2b1b9b" position={[5, -10, 5]} intensity={880} />
+			<spotLight color="#2b1b9b" position={[-10, -20, 10]} intensity={4000} />
 		</>
 	);
 }

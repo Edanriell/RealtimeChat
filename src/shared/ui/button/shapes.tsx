b889e0543e1fba0fb3aa1386e544a3427d210aa4
Edanriell <Edanriell@gmail.com>
2024-02-18 23:27:05 +0200

    need add more colors to elements

--- src/shared/ui/button/shapes.tsx
+++ src/shared/ui/button/shapes.tsx
@@ -80,17 +80,17 @@ export function Lights() {
 	useShadowHelper(refka);
 	return (
 		<>
-			{/* <spotLight color="#761beb" position={[-10, -10, -10]} intensity={220} /> */}
-			{/* <spotLight color="#5a14de" position={[-10, 0, 15]} intensity={880} /> */}
-			{/* <spotLight color="#5a14de" position={[-5, 20, 2]} intensity={550} /> */}
-			{/* <spotLight color="#5a14de" position={[15, 10, -2]} intensity={2200} /> */}
-			{/* <spotLight color="#5a14de" position={[15, 10, 5]} intensity={1100} /> */}
-			{/* <spotLight color="##761beb" position={[5, -10, 5]} intensity={880} /> */}
+			<spotLight color="#761beb" position={[-10, -10, -10]} intensity={220} />
+			<spotLight color="#5a14de" position={[-10, 0, 15]} intensity={880} />
+			<spotLight color="#5a14de" position={[-5, 20, 2]} intensity={550} />
+			<spotLight color="#5a14de" position={[15, 10, -2]} intensity={1200} />
+			<spotLight color="#5a14de" position={[15, 10, 5]} intensity={1100} />
+			<spotLight color="#5a14de" position={[5, -10, 5]} intensity={880} />
 			<spotLight
 				ref={refka}
-				color="#FFF"
-				position={[15, 10, -2]}
-				intensity={2200}
+				color="#5a14de"
+				position={[-10, -20, 10]}
+				intensity={4000}
 			/>
 		</>
 	);

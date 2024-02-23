18fa3631d4db501e997c163f54039cceac45f9c8
Edanriell <Edanriell@gmail.com>
2024-02-23 13:34:14 +0200

    refactoring add friiend

--- src/next-pages/add-friends/ui/index.tsx
+++ src/next-pages/add-friends/ui/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
-import AddFriendButton from "@/components/AddFriendButton";
+import { AddFriend } from "@/features/add-friend";
 
 export const AddFriendsPage: FC = () => {
 	return (
@@ -11,7 +11,7 @@ export const AddFriendsPage: FC = () => {
 			}
 		>
 			<h1 className="font-bold text-5xl mb-8">Add a friend</h1>
-			<AddFriendButton />
+			<AddFriend />
 		</div>
 	);
 };

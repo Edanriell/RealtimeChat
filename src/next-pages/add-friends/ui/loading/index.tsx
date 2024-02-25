cb7a236d65aa5623ee1f4690b90911c8455491aa
Edanriell <Edanriell@gmail.com>
2024-02-25 13:36:25 +0200

    fixed skeleton for add friends page

--- src/next-pages/add-friends/ui/loading/index.tsx
+++ src/next-pages/add-friends/ui/loading/index.tsx
@@ -4,10 +4,31 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 const AddFriendsPageLoading: FC = () => {
 	return (
-		<div className="w-full flex flex-col gap-3">
-			<Skeleton className="mb-4" height={60} width={500} />
-			<Skeleton height={20} width={150} />
-			<Skeleton height={50} width={400} />
+		<div
+			className={
+				"w-full h-full p-[60px] rounded-[20px] shadow-soft " +
+				"glassmorphic-element glassmorphic-element__border " +
+				"relative"
+			}
+		>
+			<h1 className={"font-bold text-5xl mb-8 text-center"}>
+				<Skeleton className={"block text-center"} width={281.56} height={48} />
+			</h1>
+			<div
+				className={
+					"absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+				}
+			>
+				<div
+					className={"flex flex-col gap-y-[20px] items-center justify-center"}
+				>
+					<div className={"flex flex-col items-center gap-y-[10px]"}>
+						<Skeleton width={131.45} height={24} />
+						<Skeleton width={208.47} height={39.99} />
+					</div>
+					<Skeleton width={123.1} height={39.98} />
+				</div>
+			</div>
 		</div>
 	);
 };
